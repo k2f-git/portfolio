90e1adb5252531def186f025afa9048f8ad2ce92
k2f-git <ferdismacbook@MacBook-Pro-de-Ferdinand-2.local>
2020-02-21 13:11:47 +0000

    JS: animation de background premiere partie

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
 const loader = document.querySelector('#loader');
-
+var back = document.querySelectorAll('.back');
+[back1,back2] = back;
 const checkLoad = ()=>{ //function qui desactive le loader une fois que la connexion est etablie
     document.addEventListener('load',()=>{
         loader.style.opacity = 0;
     });
 }
 checkLoad();
+
+const changeBackground = ()=>{
+    document.addEventListener('scroll', () => {
+        console.log(document.documentElement.scrollTop);
+        if(document.body.scrollTop > (window.innerHeight/1.2) || document.documentElement.scrollTop > (window.innerHeight/1.2)){
+            back1.classList.remove('black');
+            back1.classList.add('white');
+            back2.classList.remove('white');
+            back2.classList.add('black');
+        }
+        else if (document.body.scrollTop <= (window.innerHeight/8) || document.documentElement.scrollTop <= (window.innerHeight/8)){
+            back2.classList.remove('black');
+            back2.classList.add('white');
+            back1.classList.remove('white');
+            back1.classList.add('black');
+        }
+    })
+};
+
+changeBackground();
 
